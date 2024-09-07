8afd39b13dd2d1bc7138ce14188f3bf8ad44b620
d4vidholly <dvidhdesign@gmail.com>
2024-09-08 09:30:10 +1000

    9.30am

--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,36 @@ function myFunction() {
     console.log('test')
 }
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    console.log('test')
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Initialize the map and set its view to the chosen geographical coordinates and zoom level
+    var map = L.map('map').setView([-36.75642, 144.26456], 13);
+
+    // Add a tile layer to the map (using OpenStreetMap tiles)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // Add a marker to the map at the given coordinates
+    L.marker([-36.75642, 144.26456]).addTo(map)
+        .bindPopup('Car Crash Data 101')
+        .openPopup();
+
+        L.marker([-36.77334, 144.30782]).addTo(map)
+        .bindPopup('Car Crash Data 102')
+        .openPopup();
+
+        L.marker([-36.72944, 144.28036]).addTo(map)
+        .bindPopup('Car Crash Data 103')
+        .openPopup();
+});
+
+
+
 // window.onclick = function(event) {
 //     if (!event.target.matches('.dropbtn')) {
 //         var dropdowns = document.getElementsByClassName("dropdown-content");
